aa29581b552d652a7e55b4e66ad304ed44811d51
RCattoi <rodrigoccattoi@gmail.com>
2025-10-28 11:53:08 -0300

    feat: implement FileHandler class with methods for file operations

---
+++ utils/file_handler.py
@@ -0,0 +1,14 @@
+class FileHandler:
+    def __init__(self):
+        pass
+    
+    def read_file(self, file_path):
+        pass
+    
+    def get_sheet_names(self, file_path):
+        pass
+    
+    def save_csv(self, df, output_path):
+        pass
+    
+    
